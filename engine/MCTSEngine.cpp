07c688946c0e81d2110a2dd5c0664257c1943b13
zhengran <zhengran@kuaishou.com>
2024-02-17 19:39:41 +0800

    tcmalloc

--- engine/MCTSEngine.cpp
+++ engine/MCTSEngine.cpp
@@ -31,7 +31,7 @@ namespace gomoku {
         root_black = black_first;
         root_node_ = std::make_shared<Node>(black_first, this);
         root_board_ = state;
-        threadPool.Init(1, std::bind(&MCTSEngine::LoopExpandTree, this));
+        threadPool.Init(8, std::bind(&MCTSEngine::LoopExpandTree, this));
         threadPool.Start();
         return true;
     }
